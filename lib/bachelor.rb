4c481b20be4e46891dcd7c360c0c337ed30504e7
Qun Li Huang <qunli02@gmail.com>
2019-04-15 17:04:37 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -23,10 +23,10 @@ def get_contestant_name(data, occupation)
         name = stuff
       end
       if desc == "occupation" && stuff == occupation
-        print name
       end
     end
   end
+  print name
 end
 
 def count_contestants_by_hometown(data, hometown)
