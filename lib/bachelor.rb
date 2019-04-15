8481f1c6aaf598e0f35c024a04d6781e4f9935f5
Qun Li Huang <qunli02@gmail.com>
2019-04-15 17:03:51 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -23,7 +23,7 @@ def get_contestant_name(data, occupation)
         name = stuff
       end
       if desc == "occupation" && stuff == occupation
-        return name
+        print name
       end
     end
   end
